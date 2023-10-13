7b7822af6e14514077e602e4860faeadb629380d
Shashi raj <shashiraj13457@gmail.com>
2023-10-13 17:52:00 +0530

    Basic Concept

---
+++ basics.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //Array Creation
+
+    // int arr[101];
+    // char arr[20];
+    // bool arr[250];
+    // long num[1000];
+    // short snum[12];
+
+    // cout<<"Array Created Successfully"<<endl;
+
+    // int a = 5;
+    // cout<<"Size of a: "<<sizeof(a)<<endl;
+    // cout<< "Address of a: "<< &a <<endl;
+
+    // int arr[10];
+    // cout<<"Size of arr: "<<sizeof(arr)<<endl;
+    // cout<< "Base address of Arr is: "<< &arr <<endl;
+    // cout<< "Base address of Arr is: "<< arr <<endl;
+
+    //Array Initialization
+
+    // int arr[] = {2,3,4,5,6};
+    // int brr[5] = {5,8,5,9,9}; 
+    // int crr[5] = {5,8}; 
+    // int drr[2] = {5,8,5,9,9}; //error
+    
+    return 0;
+}
