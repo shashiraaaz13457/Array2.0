73f070e4ad34d86343c0dd8746dd3df3d646a431
Shashi raj <shashiraj13457@gmail.com>
2023-11-09 10:16:15 +0530

    Questions

--- Shift_arrayElement.cpp
+++ Shift_arrayElement.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-
-///right shift
+//Solve this concepts own your own
+//right shift
 void ShiftElements(int arr[], int n){
     int temp = arr[n-1]; 
     for (int i = n-1; i > 0; i--)
@@ -12,11 +12,23 @@ void ShiftElements(int arr[], int n){
     arr[0] = temp;
     
 }
+//Left Shift
+void ShiftElements2(int arr[], int n){
+    int temp = arr[0]; 
+    for (int i = 0; i < n; i++)
+    {
+        arr[i]= arr[i+1];
+    }
+    //copy temp to the first index.
+    arr[n-1] = temp;
+    
+}
 int main(){
     int arr[] = {10,20,30,40,50,60};
     int n = 6;
 
-    ShiftElements(arr,n);
+    ShiftElements2(arr,n);
+    // ShiftElements(arr,n);
 
     //print array
     for (int i = 0; i < n; i++)
