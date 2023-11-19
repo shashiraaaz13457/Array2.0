6a85ac181499163c1c7e6050dcdf7f8f37435108
Shashi raj <shashiraj13457@gmail.com>
2023-11-19 22:49:40 +0530

    Revision

---
+++ Revision.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//     //Array Creation + Array Initialisation
+//     // int arr[5] = {20,25,60,78,90};
+//     int arr[5];
+
+//     //Taking input in array
+//     int n = 5;
+//     cout<<"Enter the value: "<<endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     cout<<"{";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<arr[i]<<",";
+//     }
+//     cout<<"}";
+
+//1st problem statement
+    // int arr[10];
+    int n = 6;
+    // cout<<"Enter the value: ";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<2*arr[i]<<endl;
+    // }
+//2nd problem statement(Linear Search Concept)
+    // int arr[6] = {2,4,6,8,10,12};
+    // int target = 11;
+    // bool flag = 0;
+    // //0 ->not found
+    // //1 ->found
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] == target)
+    //     {
+    //         flag = 1;
+    //         break;
+    //     }
+        
+    // }
+    // if (flag == 1)
+    // {
+    //     cout<<"Element found";
+    // }
+    // else{
+    //     cout<<"Element not found";
+    // }
+    
+    
+    
+    
+    
+}
