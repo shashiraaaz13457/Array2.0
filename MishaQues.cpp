745c7698e5964bc448977c27821376de8211530a
Shashi raj <shashiraj13457@gmail.com>
2023-11-23 19:14:01 +0530

    Question

--- MishaQues.cpp
+++ MishaQues.cpp
@@ -5,14 +5,14 @@ using namespace std;
 //don't repeat the value inside array
 
 int main(){
-    int arr[7] = {1,1,1,3,5,4,5}; // 1,2,3,4,5
+    int arr[7] = {1,1,1,3,5,4,5}; // 1,3,4,5
     int n = 7, count = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = i+1; j < n; j++)
         {
             if (arr[i]==arr[j]){
-                
+
             }
         }
 
